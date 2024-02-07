5a0cdeb6145ac2f907a46481719d4ffdf8a8e3d1
JARJAR1028 <jribando@nyit.edu>
2024-02-07 15:41:12 -0500

    Added the session screen, which appears after the submit button is pressed in PresessionQuestions
    Bug with pressing buttons in landscape mode

--- app/src/main/java/com/example/vrexpo/PresessionQuestions.java
+++ app/src/main/java/com/example/vrexpo/PresessionQuestions.java
@@ -2,13 +2,36 @@ package com.example.vrexpo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class PresessionQuestions extends AppCompatActivity {
+
+    private static final String TAG = "VRExpo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presession_questions);
+
+        //Find the buttons
+        Button submitBtn = findViewById(R.id.submitButton);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "Submit button clicked");
+                Toast.makeText(PresessionQuestions.this, "Submit button clicked", Toast.LENGTH_SHORT)
+                        .show();
+
+                //open postsession questions
+                Intent sessionIntent = new Intent(PresessionQuestions.this, Session.class);
+                startActivity(sessionIntent);
+            }
+        });
     }
 }
