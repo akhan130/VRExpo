a339eebfe301818c26e8ec7693a0d237aa252649
JARJAR1028 <jribando@nyit.edu>
2024-03-30 13:56:55 -0400

    Picking a date is now formatted

--- app/src/main/java/com/example/vrexpo/PatientInfo.java
+++ app/src/main/java/com/example/vrexpo/PatientInfo.java
@@ -4,13 +4,23 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class PatientInfo extends AppCompatActivity {
+import java.util.Calendar;
 
+public class PatientInfo extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnDatePicker;
+    EditText txtDate;
+    private int mYear, mMonth, mDay;
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //Inflate the menu
@@ -55,8 +65,37 @@ public class PatientInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_info);
 
+        btnDatePicker = (Button)findViewById(R.id.btn_date);
+        txtDate = (EditText)findViewById(R.id.in_date);
+        btnDatePicker.setOnClickListener(this);
+
         //Setting up the action bar
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+    }
+    public void onClick(View v) {
+
+        if (v == btnDatePicker) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
     }
 }
