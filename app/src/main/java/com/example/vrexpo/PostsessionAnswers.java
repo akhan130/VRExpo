55cc81a59e2adb0c9b9b4a47b276e574146a9668
JARJAR1028 <jribando@nyit.edu>
2024-04-29 11:32:16 -0400

    Added UI to retrieve answers from the pre and post session questionaires

---
+++ app/src/main/java/com/example/vrexpo/PostsessionAnswers.java
@@ -0,0 +1,14 @@
+package com.example.vrexpo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostsessionAnswers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postsession_answers);
+    }
+}
