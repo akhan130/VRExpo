c6bda95babdb6e85a208cb887c149196b6a66b8f
riavarghese21 <riavarghese4@gmail.com>
2024-05-03 21:37:55 -0400

    Modified View Pre and Post Session Questions for Therapist and Patient

--- app/src/main/java/com/example/vrexpo/PatientRecords.java
+++ app/src/main/java/com/example/vrexpo/PatientRecords.java
@@ -1,37 +1,122 @@
 package com.example.vrexpo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
 
 public class PatientRecords extends AppCompatActivity {
+
+    private ToggleButton toggleButton;
+    private RelativeLayout preSessionContainer, postSessionContainer;
+
+    Button btnDatePicker;
+    EditText txtDate;
+    private int mYear, mMonth, mDay;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu
+        getMenuInflater().inflate(R.menu.dashboard_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_dashboard:
+                Intent dashIntent = new Intent(PatientRecords.this, Dashboard.class);
+                startActivity(dashIntent);
+                return true;
+            case R.id.action_sessionStart:
+                Intent zoom = new Intent(PatientRecords.this, SessionStart.class);
+                startActivity(zoom);
+                return true;
+            case R.id.action_accountInfo:
+                Intent actInfoIntent = new Intent(PatientRecords.this, AccountInfo.class);
+                startActivity(actInfoIntent);
+                return true;
+            case R.id.action_appointments:
+                Intent appointments = new Intent(PatientRecords.this, PatientAppointments.class);
+                startActivity(appointments);
+                return true;
+            case R.id.action_find_therapist:
+                Intent findIntent = new Intent(PatientRecords.this, FindTherapist.class);
+                startActivity(findIntent);
+                return true;
+            case R.id.action_messages:
+                Intent messages = new Intent(PatientRecords.this, PatientMessages.class);
+                startActivity(messages);
+                return true;
+            case R.id.action_patient_settings:
+                Intent settingsIntent = new Intent(PatientRecords.this, PatientSettings.class);
+                startActivity(settingsIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_patient_records);
+        setContentView(R.layout.activity_view_patient_questions);
 
-        Button PresessionAnswersButton = findViewById(R.id.presession_answers);
-        Button PostsessionAnswersButton = findViewById(R.id.postsession_answers);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
-        PresessionAnswersButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //open history questions
-                Intent presessionAnswersIntent = new Intent(PatientRecords.this, PresessionAnswers.class);
-                startActivity(presessionAnswersIntent);
+        preSessionContainer = findViewById(R.id.presessionContainer);
+        postSessionContainer = findViewById(R.id.postsessionContainer);
+        toggleButton = findViewById(R.id.toggleButton);
+
+        toggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                // Post-session questions visible
+                postSessionContainer.setVisibility(View.VISIBLE);
+                preSessionContainer.setVisibility(View.GONE);
+            } else {
+                // Pre-session questions visible
+                preSessionContainer.setVisibility(View.VISIBLE);
+                postSessionContainer.setVisibility(View.GONE);
             }
         });
-        PostsessionAnswersButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //open history questions
-                Intent postsessionAnswersIntent = new Intent(PatientRecords.this, PostsessionAnswers.class);
-                startActivity(postsessionAnswersIntent);
-            }
-        });
+    }
+
+    public void onClick(View v) {
+
+        if (v == btnDatePicker) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
     }
 }
--- app/src/main/java/com/example/vrexpo/ViewPatientQuestions.java
+++ app/src/main/java/com/example/vrexpo/ViewPatientQuestions.java
@@ -4,14 +4,28 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
 
 public class ViewPatientQuestions extends AppCompatActivity {
+
+    private ToggleButton toggleButton;
+    private RelativeLayout preSessionContainer, postSessionContainer;
+
+    Button btnDatePicker;
+    EditText txtDate;
+    private int mYear, mMonth, mDay;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -21,7 +35,7 @@ public class ViewPatientQuestions extends AppCompatActivity {
     }
 
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_home:
                 Intent dashIntent = new Intent(ViewPatientQuestions.this, TherapistDashboard.class);
                 startActivity(dashIntent);
@@ -67,24 +81,46 @@ public class ViewPatientQuestions extends AppCompatActivity {
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        Button presessionQuestions = findViewById(R.id.presessionButton);
-        Button postsessionQuestions = findViewById(R.id.postsessionButton);
+        preSessionContainer = findViewById(R.id.presessionContainer);
+        postSessionContainer = findViewById(R.id.postsessionContainer);
+        toggleButton = findViewById(R.id.toggleButton);
 
-        presessionQuestions.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent therapistInfoIntent = new Intent(ViewPatientQuestions.this, ViewPatientPresession.class);
-                startActivity(therapistInfoIntent);
+        toggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                // Post-session questions visible
+                postSessionContainer.setVisibility(View.VISIBLE);
+                preSessionContainer.setVisibility(View.GONE);
+            } else {
+                // Pre-session questions visible
+                preSessionContainer.setVisibility(View.VISIBLE);
+                postSessionContainer.setVisibility(View.GONE);
             }
         });
+    }
 
-        postsessionQuestions.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent therapistInfoIntent = new Intent(ViewPatientQuestions.this, ViewPatientPostsession.class);
-                startActivity(therapistInfoIntent);
-            }
-        });
+    public void onClick(View v) {
 
+        if (v == btnDatePicker) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
     }
 }
